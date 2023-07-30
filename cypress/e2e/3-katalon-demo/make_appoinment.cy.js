05dfdb9e5d3e6a12970d0f5811222a539ec3b79a
prayogoferry <prayogoferry@gmail.com>
2023-07-30 22:33:52 +0700

    appoinment

--- cypress/e2e/3-katalon-demo/make_appoinment.cy.js
+++ cypress/e2e/3-katalon-demo/make_appoinment.cy.js
@@ -8,8 +8,16 @@ describe('Make Appointment', ()=>{
     const facility = ["Tokyo CURA Healthcare Center", "Hongkong CURA Healthcare Center","Seoul CURA Healthcare Center"]
     const program = ["#radio_program_medicare", "#radio_program_medicaid", "#radio_program_none"]
     let comment = "description"
+    
     it('Apointment schema 1', () => {
-        cy.appointment(facility[0], program[2], comment)
+        cy.appointment(facility[0], program[1], comment)
+
+        
+         cy.get('#facility').should('contain', facility[0])
+         cy.get('#hospital_readmission').should('contain', 'Yes')
+         cy.get('#program').should('contain', "Medicaid")
+         cy.get('#comment').should('contain', comment)
+
     });
     
 })
