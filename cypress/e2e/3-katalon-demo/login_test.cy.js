ed167016342a403575b8ab067ca76e9541bf4a0d
prayogoferry <prayogoferry@gmail.com>
2023-07-30 21:40:10 +0700

    appoinment

--- cypress/e2e/3-katalon-demo/login_test.cy.js
+++ cypress/e2e/3-katalon-demo/login_test.cy.js
@@ -1,5 +1,37 @@
 /// <reference types = "cypress"/>
 
 beforeEach(()=>{
-    cy.load_katalon()
+    cy.visit('https://katalon-demo-cura.herokuapp.com/')
+    cy.load_dkatalon()
+})
+
+describe('Login Test',()=>{
+    it('Login use valid data', () => {
+        cy.get("@data_katalon").then(dt_katalon=>{
+           
+            cy.loginKtln(dt_katalon.username, dt_katalon.password)
+        })
+
+        cy.log("Assertion Success Login")
+        cy.url().should('include', '/#appointment')
+        cy.get('h2').should('be.visible')
+    });
+
+    it('Login use invalid data', () => {
+        cy.loginKtln('usename', 'password')
+
+        cy.log('assertion loginpage failed')
+        cy.url().should('contain', '/profile.php#login')
+        cy.get('.text-danger').should('be.visible').should('contain', 'Login failed')
+    });
+
+    it('Try Logout', () => {
+        cy.login_success()
+
+        cy.get('#menu-toggle').click()
+        cy.get(':nth-child(6) > a').click()
+
+        cy.log("Logout Assertion")
+        cy.url().should('contain', 'katalon-demo-cura.herokuapp.com')
+    });
 })
--- cypress/support/katalon_demo.js
+++ cypress/support/katalon_demo.js
@@ -3,9 +3,21 @@ Cypress.Commands.add('load_dkatalon', ()=>{
     cy.fixture("katalon_demo").as('data_katalon')
 })
 
-Cypress.Commands.add('loginStep', (username, password)=>{
-
+Cypress.Commands.add('loginKtln', (username, password)=>{
+    cy.get('#btn-make-appointment').click()
+    cy.log('assertion loginpage')
+    cy.url().should('contain', '/profile.php#login')
     cy.get('#txt-username').clear().type(username)
     cy.get('#txt-password').clear().type(password)
-    cy.contains('submit').click()
+    cy.get('#btn-login').click()
+})
+
+Cypress.Commands.add('login_success',()=>{
+    cy.fixture("katalon_demo").then(dt_katalon=>{
+    
+    cy.get('#btn-make-appointment').click()
+    cy.get('#txt-username').clear().type(dt_katalon.username)
+    cy.get('#txt-password').clear().type(dt_katalon.password)
+    cy.get('#btn-login').click()
+    })
 })
