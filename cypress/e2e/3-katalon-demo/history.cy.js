f7d6fda80ff98767579c03c558ee9053fce622e5
prayogoferry <prayogoferry@gmail.com>
2023-07-30 22:51:52 +0700

    History

--- cypress/e2e/3-katalon-demo/history.cy.js
+++ cypress/e2e/3-katalon-demo/history.cy.js
@@ -9,8 +9,25 @@ describe('History Test', ()=>{
     const program = ["#radio_program_medicare", "#radio_program_medicaid", "#radio_program_none"]
     let comment = "description"
     
-    it('History Schema 1', () => {
+    it('History Schema with no appointment ', () => {
         cy.get('#menu-toggle').click()
         cy.get(':nth-child(4) > a').click()
+
+        cy.url().should('contain', "/history.php#history")
+        cy.get('h2').should('be.visible').should('contain', "History")
+        cy.get('.col-sm-12 > p').should('be.visible').should('contain', "No appointment.")
     });
+
+    it('History Schema with appointment ', () => {
+
+        cy.appointment(facility[0], program[1], comment)
+        cy.get('#menu-toggle').click()
+        cy.get(':nth-child(4) > a').click()
+
+        cy.url().should('contain', "/history.php#history")
+        cy.get('.panel-heading').should('be.visible')
+        cy.get('h2').should('be.visible').should('contain', "History")
+    });
+
+
 })
